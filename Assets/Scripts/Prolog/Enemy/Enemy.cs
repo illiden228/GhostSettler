7342c5615d9892712be2f36122b142880035d5ef
Денис <den.khabibullin@gmail.com>
2020-03-31 17:17:17 +0300

    Основная механика пролога: игрок, враги, изменение прозрачности и переход на следующий уровень. Пролог будет как отдельная часть от основной механики игры

---
+++ Assets/Scripts/Prolog/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Player>(out Player player))
+        {
+            player.ApplyDamage(_damage);
+        }
+
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Prolog/Enemy/EnemyMover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private void Update()
+    {
+        transform.Translate(Vector2.up * _speed * Time.deltaTime);
+    }
+}
